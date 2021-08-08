fbe2e8d95c845b33ccd339818b9a7d5298b731c5
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-08-08 16:21:48 +0300

    db - added admin table and view that unifies it with customers

---
+++ java_workshop_db/sql_script_admins.sql
@@ -0,0 +1,53 @@
+USE java_workshop_db;
+
+
+DROP TRIGGER trigger_admins_update;
+DROP TABLE admins;
+
+
+CREATE TABLE admins (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(320) NOT NULL UNIQUE,
+    pass VARBINARY(256) NOT NULL,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    phone VARCHAR(20),
+    address TEXT,
+	created TIMESTAMP DEFAULT NOW(),
+	modified TIMESTAMP DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
+ 
+CREATE TRIGGER trigger_admins_update
+BEFORE UPDATE ON admins
+FOR EACH ROW
+SET NEW.modified = NOW();
+
+# test category
+INSERT INTO admins (email, pass, first_name, last_name) VALUES ('admin2@admin.admin', 'mysuperpass', 'admin2', 'administrator2');
+
+#test update
+# UPDATE customers  SET name = 'cat2' WHERE id = 1;
+
+# delete all from table
+DELETE FROM admins WHERE id > 0;
+
+
+# will have paging - the LIMIT can be used (if it gets 2 values then the first is the number to start AFTER and the second is the amount)
+
+
+SELECT * FROM admins;
+
+
+
+# view that consolidates customers and admins together as users (used for login)
+
+CREATE VIEW users AS
+SELECT *, 0 AS is_admin FROM customers
+UNION
+SELECT *, 1 AS is_admin FROM admins;
+
+
+SELECT * FROM users;
+
+
