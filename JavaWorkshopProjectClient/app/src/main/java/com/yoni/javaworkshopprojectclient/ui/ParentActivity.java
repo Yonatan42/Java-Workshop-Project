a2973ac699a1e0ff533b924bbeef8b6359917be8
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-08-20 17:44:47 +0300

    client - added tabs ui

--- JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/ParentActivity.java
+++ JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/ParentActivity.java
@@ -1,20 +1,43 @@
 package com.yoni.javaworkshopprojectclient.ui;
 
+import androidx.annotation.IntDef;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
 import com.yoni.javaworkshopprojectclient.R;
 import com.yoni.javaworkshopprojectclient.localdatastores.cart.CartStore;
 import com.yoni.javaworkshopprojectclient.localdatastores.TokenStore;
 import com.yoni.javaworkshopprojectclient.ui.fragments.BaseFragment;
 import com.yoni.javaworkshopprojectclient.ui.fragments.SplashFragment;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class ParentActivity extends AppCompatActivity {
 
+    public static final int TAB_PRODUCTS = 0;
+    public static final int TAB_ORDERS = 1;
+    public static final int TAB_CART = 2;
+    public static final int TAB_PROFILE = 3;
+    public static final int TAB_ADMIN = 4;
+
+    @IntDef({TAB_PRODUCTS, TAB_ORDERS, TAB_CART, TAB_PROFILE, TAB_ADMIN})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TabIndex {}
+
+
+
     private FragmentManager fragmentManager;
+    private TabLayout tabLayout;
 
 
     @Override
@@ -24,6 +47,7 @@ public class ParentActivity extends AppCompatActivity {
 
         initializeLocalStores();
         initializeFragments();
+        initializeTabLayout();
     }
 
     private void initializeLocalStores(){
@@ -39,6 +63,19 @@ public class ParentActivity extends AppCompatActivity {
                 .commit();
     }
 
+    private void initializeTabLayout(){
+        tabLayout = findViewById(R.id.main_tab_layout);
+
+        ViewGroup tabs = ((ViewGroup) tabLayout.getChildAt(0));
+        for (int i = 0; i < tabs.getChildCount(); i++){
+            tabLayout.getTabAt(i).getIcon().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+        }
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, android.R.color.transparent));
+        tabLayout.setTabRippleColorResource(android.R.color.darker_gray);
+
+        setAdminTabVisible(false);
+    }
+
     public void makeFragmentTransition(BaseFragment frag){
         makeFragmentTransition(frag, true);
     }
@@ -49,5 +86,14 @@ public class ParentActivity extends AppCompatActivity {
             transaction.addToBackStack(null);
         }
         transaction.commit();
+    }
+
+    public void setAdminTabVisible(boolean isVisible){
+        int visibility = isVisible ? View.VISIBLE : View.GONE;
+        ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(TAB_ADMIN).setVisibility(visibility);
+    }
+
+    public void setSelectedTab(@TabIndex int tabIndex){
+        tabLayout.selectTab(tabLayout.getTabAt(tabIndex));
     }
 }
