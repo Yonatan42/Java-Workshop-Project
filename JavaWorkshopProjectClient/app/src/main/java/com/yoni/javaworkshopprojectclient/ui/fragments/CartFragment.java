05f6b6a2ad9ae76577a4ddcccee8ad4d883a884d
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-08-22 22:24:24 +0300

    client - added placeholder for cart

--- JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/fragments/CartFragment.java
+++ JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/fragments/CartFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.yoni.javaworkshopprojectclient.R;
+import com.yoni.javaworkshopprojectclient.datatransfer.models.entitymodels.CartProduct;
+import com.yoni.javaworkshopprojectclient.localdatastores.cart.CartStore;
+
+import java.util.List;
 
 public class CartFragment extends BaseFragment {
 
@@ -24,5 +29,18 @@ public class CartFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        List<CartProduct> products = CartStore.getInstance().getAll();
+
+        // todo - remove this, it's just for testing
+
+        TextView txtData = view.findViewById(R.id.cart_txt);
+
+        StringBuilder sb = new StringBuilder();
+        for (CartProduct product: products) {
+            sb.append(product.getProductId()).append("\t\t\t\t").append(product.getQuantity()).append("\n");
+        }
+        txtData.setText(sb);
+
     }
 }
