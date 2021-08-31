9c12ca8ed684f651d7848f1fd8d0382b6575996d
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-09-01 01:32:18 +0300

    client - added register action

--- JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/screenfragments/RegisterFragment.java
+++ JavaWorkshopProjectClient/app/src/main/java/com/yoni/javaworkshopprojectclient/ui/screenfragments/RegisterFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.yoni.javaworkshopprojectclient.R;
+import com.yoni.javaworkshopprojectclient.datatransfer.ServerResponse;
+import com.yoni.javaworkshopprojectclient.remote.RemoteServiceManager;
+import com.yoni.javaworkshopprojectclient.ui.areafragments.ProfileDetailsFragment;
+import com.yoni.javaworkshopprojectclient.ui.popups.ErrorPopup;
+import com.yoni.javaworkshopprojectclient.utils.AppScreen;
 
 public class RegisterFragment extends BaseFragment {
 
@@ -26,5 +32,40 @@ public class RegisterFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
 
         getParentActivity().setTabBarVisibility(false);
+
+        Button btnReg = view.findViewById(R.id.register_btn_register);
+        Button btnBack = view.findViewById(R.id.register_btn_back);
+        ProfileDetailsFragment profileDetailsFrag = (ProfileDetailsFragment) getChildFragmentManager().findFragmentById(R.id.register_profile_details_fragment);
+
+        btnBack.setOnClickListener(v -> getParentActivity().onBackPressed());
+
+        btnReg.setOnClickListener(v -> {
+            if(!profileDetailsFrag.validate(true)){
+                return;
+            }
+
+            attemptRegister(profileDetailsFrag);
+        });
     }
+
+    private void attemptRegister(ProfileDetailsFragment profileDetailsFrag) {
+        profileDetailsFrag.setIsEditable(false);
+        RemoteServiceManager.getInstance().getUsersService().register(
+                profileDetailsFrag.getEmail(),
+                profileDetailsFrag.getPassword(),
+                profileDetailsFrag.getFirstName(),
+                profileDetailsFrag.getLastName(),
+                profileDetailsFrag.getPhone(),
+                profileDetailsFrag.getAddress(),
+                (call, response, result) -> getParentActivity().makeFragmentTransition(AppScreen.PRODUCTS.getFragment(), false),
+                (call, responseError) -> {
+                    if (responseError.getCode() == ServerResponse.ServerResponseError.UNKNOWN_ERROR_CODE) {
+                        new ErrorPopup(getContext(), getString(R.string.error_check_internet), () -> attemptRegister(profileDetailsFrag)).show();
+                    }
+                    ErrorPopup.createGenericOneOff(getParentActivity()).show();
+                    profileDetailsFrag.setIsEditable(true);
+                }
+        );
+    }
+
 }
