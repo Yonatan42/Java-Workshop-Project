1772f88dd67b6e64a75360eaf6a06b95727fffbc
Yonatan Giventer <yonatan.giventer@gmail.com>
2021-09-09 19:10:01 +0300

    server - added constraint violation check to base sevice

--- JavaWorkshopProjectServer/src/main/java/com/yoni/javaworkshopprojectserver/service/BaseService.java
+++ JavaWorkshopProjectServer/src/main/java/com/yoni/javaworkshopprojectserver/service/BaseService.java
@@ -1,10 +1,14 @@
 package com.yoni.javaworkshopprojectserver.service;
 
 import com.yoni.javaworkshopprojectserver.EntityManagerSingleton;
+import com.yoni.javaworkshopprojectserver.utils.Logger;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -15,8 +19,14 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 public abstract class BaseService {
+
+    private static final String TAG = "BaseService";
 
     @EJB
     private EntityManagerSingleton entityManagerBean;
@@ -44,10 +54,32 @@ public abstract class BaseService {
         return list.get(0);
     }
 
-    public void withTransaction(Runnable action){
+    protected void withTransaction(Runnable action){
         getEntityManager().getTransaction().begin();
         action.run();
         getEntityManager().getTransaction().commit();
+    }
+
+    protected <T> void withValidation(T entity, Consumer<T> action){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<T>> constraintViolations = validator.validate(entity);
+        if(constraintViolations.size() > 0){
+            StringBuilder constraintViolationMsg = new StringBuilder();
+            for (ConstraintViolation<T> violation : constraintViolations) {
+                constraintViolationMsg
+                        .append('\n')
+                        .append("violation: ")
+                        .append(violation.getRootBeanClass().getName())
+                        .append(".")
+                        .append(violation.getPropertyPath())
+                        .append(" ")
+                        .append(violation.getMessage());
+            }
+            Logger.logError(TAG, "Constraint Violations: "+constraintViolationMsg);
+        }else{
+            action.accept(entity);
+        }
     }
 
 }
